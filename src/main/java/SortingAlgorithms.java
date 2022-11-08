2d5b9cb5fba3c1eaa642c2314b30d4e2bec4aacc
Anas Lamaiz <lamaizanass@gmail.com>
2022-11-08 19:42:51 +0100

    sorting algortithms

---
+++ src/main/java/SortingAlgorithms.java
@@ -0,0 +1,18 @@
+public class SortingAlgorithms {
+
+    public static void bubble(int[] list) {
+
+    }
+
+    public static void insertion(int[] list) {
+
+    }
+
+    public static void selection(int[] list) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
